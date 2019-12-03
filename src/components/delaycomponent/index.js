69ee2f282d0d2ed3b53a573153a539b8847bf1ec
Vivek Vaibhav <vivek@oriserve.com>
2019-12-03 15:40:55 +0530

    alignment fixed

--- src/components/delaycomponent/index.js
+++ src/components/delaycomponent/index.js
@@ -2,35 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const DelayComponent = (Component) => {
-    return class extends React.Component {
-        state = {
-            shouldRender: this.props.isMounted
-        }
-
-        componentWillReceiveProps(nextProps) {
-            const { isMounted, delayMountTime, delayUnmountTime } = this.props;
-            if ((isMounted && !nextProps.isMounted)) {
-                setTimeout(() => this.setState({ shouldRender: false }), delayUnmountTime)
-            } else if (!isMounted && nextProps.isMounted) {
-                setTimeout(() => this.setState({ shouldRender: true }), delayMountTime)
-            }
-        }
-
-        render() {
-            return this.state.shouldRender ? <Component {...this.props} /> : null
-        }
+  return class extends React.Component {
+    state = {
+      shouldRender: this.props.isMounted
     }
+
+    componentWillReceiveProps(nextProps) {
+      const { isMounted, delayMountTime, delayUnmountTime } = this.props;
+      if ((isMounted && !nextProps.isMounted)) {
+        setTimeout(() => this.setState({ shouldRender: false }), delayUnmountTime)
+      } else if (!isMounted && nextProps.isMounted) {
+        setTimeout(() => this.setState({ shouldRender: true }), delayMountTime)
+      }
+    }
+
+    render() {
+      return this.state.shouldRender ? <Component {...this.props} /> : null
+    }
+  }
 }
 
 DelayComponent.propTypes = {
-    delayMountTime: PropTypes.number,
-    delayUnmountTime: PropTypes.number,    
-    isMounted: PropTypes.bool
+  delayMountTime: PropTypes.number,
+  delayUnmountTime: PropTypes.number,
+  isMounted: PropTypes.bool
 }
 
 DelayComponent.defaultProps = {
-    delayMountTime: 0,
-    delayUnmountTime: 0,
+  delayMountTime: 0,
+  delayUnmountTime: 0,
 }
 
 export default DelayComponent;
--- src/components/dotsloader/index.js
+++ src/components/dotsloader/index.js
@@ -3,25 +3,25 @@ import PropTypes from 'prop-types';
 
 import './index.scss';
 
-class DotsLoader extends React.PureComponent{ 
-    render() {
-        const {dot_color} = this.props;
-        return (
-            <div className="ori-flex-row oriDotsLoaderContainer">
-                <div className={`dot one ${dot_color}`}>&nbsp;</div>
-                <div className={`dot two ${dot_color}`}>&nbsp;</div>
-                <div className={`dot three ${dot_color}`}>&nbsp;</div>
-            </div>
-        );
-    };
+class DotsLoader extends React.PureComponent {
+  render() {
+    const { dot_color } = this.props;
+    return (
+      <div className="ori-flex-row oriDotsLoaderContainer">
+        <div className={`dot one ${dot_color}`}>&nbsp;</div>
+        <div className={`dot two ${dot_color}`}>&nbsp;</div>
+        <div className={`dot three ${dot_color}`}>&nbsp;</div>
+      </div>
+    );
+  };
 }
 
 DotsLoader.propTypes = {
-    dot_color: PropTypes.string,
+  dot_color: PropTypes.string,
 };
 
 DotsLoader.defaultProps = {
-    dot_color: "ori-bg-primary",
+  dot_color: "ori-bg-primary",
 }
 
 export default DotsLoader;
