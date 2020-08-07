7091d307296ebd363e4cbe3c3c7f0fe8ca00b475
Vivek Vaibhav <vivek@oriserve.com>
2020-08-07 12:04:01 +0530

    dotsLoader updated

--- src/components/dotsloader/index.js
+++ src/components/dotsloader/index.js
@@ -3,25 +3,22 @@ import PropTypes from 'prop-types';
 
 import './index.scss';
 
-class DotsLoader extends React.PureComponent {
-  render() {
-    const { dot_color } = this.props;
-    return (
-      <div className="ori-flex-row oriDotsLoaderContainer">
-        <div className={`dot one ${dot_color}`}>&nbsp;</div>
-        <div className={`dot two ${dot_color}`}>&nbsp;</div>
-        <div className={`dot three ${dot_color}`}>&nbsp;</div>
-      </div>
-    );
-  };
+const DotsLoader = ({ container_class }) => {
+  return (
+    <div className={"oriDotsLoaderContainer " + container_class}>
+      <span className={`dot one ori-bg-primary`}>&nbsp;</span>
+      <span className={`dot two ori-bg-primary`}>&nbsp;</span>
+      <span className={`dot three ori-bg-primary`}>&nbsp;</span>
+    </div>
+  )
 }
 
 DotsLoader.propTypes = {
-  dot_color: PropTypes.string,
+  container_class: PropTypes.string
 };
 
 DotsLoader.defaultProps = {
-  dot_color: "ori-bg-primary",
+  container_class: "",
 }
 
 export default DotsLoader;
