71cca0d55d160aa281991431815a63e80824ce7a
Vivek Vaibhav <vivek@oriserve.com>
2020-08-18 10:23:25 +0530

    header updated

--- src/modules/chatbot/components/Header/index.js
+++ src/modules/chatbot/components/Header/index.js
@@ -9,48 +9,47 @@ import { logo } from '../../../../data/assets';
 
 import { LangContext } from '../../../context'
 
-const Header = React.memo(({ is_internet_connected, is_socket_connected }) => {
-  return (
-    <LangContext.Consumer>
-      {
-        lang => {
-          // const status = 
-          return (
-            <div
-              className="ori-overflow-hidden ori-bg-header ori-box-shadow-dark"
-              style={{
-                padding: '15px 45px 15px 20px',
-                height: '70px',
-              }}
-            >
-              <div className="ori-flex-row">
-                <Avatar src={logo} size="large" />
-                <div className="ori-l-pad-10 ori-flex-column ori-flex-jc" style={{ lineHeight: 1.3 }}>
-                  <p className="ori-capitalize ori-font-md ori-font-medium ori-no-b-mrgn ori-font-header">{translator.text[lang].brandName}
-                  </p>
-                  <span className="ori-font-header-light ori-font-xs">
-                    <span
-                      className={classNames(
-                        "ori-r-mrgn-5 ori-border-circle ori-display-inline-block ",
-                        {
-                          "ori-bg-danger": !is_internet_connected,
-                          "ori-bg-warning": is_internet_connected && !is_socket_connected,
-                          "ori-bg-green": is_internet_connected && is_socket_connected
-                        }
-                      )}
-                      style={{ verticalAlign: 'middle', height: '6px', width: '6px' }}
-                    />
-                    {!is_internet_connected ? translator.text[lang].offline : (!is_socket_connected ? translator.text[lang].connecting : translator.text[lang].online)}
-                  </span>
-                </div>
-              </div>
+const Header = React.memo(({ is_internet_connected, is_socket_connected }) => (
+  <LangContext.Consumer>
+    {
+      lang => (
+        <div
+          className="ori-overflow-hidden ori-bg-header ori-box-shadow-dark"
+          style={{
+            padding: '15px 45px 15px 20px',
+            height: '70px',
+          }}
+        >
+          <div className="ori-flex-row">
+            <Avatar src={logo} size="large" />
+            <div className="ori-l-pad-10 ori-flex-column ori-flex-jc" style={{ lineHeight: 1.3 }}>
+              <p className="ori-capitalize ori-font-md ori-font-medium ori-no-b-mrgn ori-font-header">{translator.text[lang].brandName}
+              </p>
+              <span className="ori-font-header-light ori-font-xs">
+                <span
+                  className={classNames(
+                    "ori-r-mrgn-5 ori-border-circle ori-display-inline-block ",
+                    {
+                      "ori-bg-danger": !is_internet_connected,
+                      "ori-bg-warning": is_internet_connected && !is_socket_connected,
+                      "ori-bg-green": is_internet_connected && is_socket_connected
+                    }
+                  )}
+                  style={{
+                    verticalAlign: 'middle',
+                    height: '6px',
+                    width: '6px'
+                  }}
+                />
+                {!is_internet_connected ? translator.text[lang].offline : (!is_socket_connected ? translator.text[lang].connecting : translator.text[lang].online)}
+              </span>
             </div>
-          )
-        }
-      }
-    </LangContext.Consumer>
-  )
-})
+          </div>
+        </div>
+      )
+    }
+  </LangContext.Consumer>
+))
 
 const mapStateToProps = state => {
   return {
