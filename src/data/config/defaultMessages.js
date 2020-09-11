991162b6c5121ed51f533b3a22665333a2077d89
Vivek Vaibhav <vivek@oriserve.com>
2020-09-11 13:10:11 +0530

    defaultMessages updated

--- src/data/config/defaultMessages.js
+++ src/data/config/defaultMessages.js
@@ -2,10 +2,10 @@ import { MESSAGE_SENDER, MESSAGE_TYPES, BUTTON_TYPES } from './constants'
 
 export const web = [
   {
-    sender: MESSAGE_SENDER.CHATBOT,
     type: MESSAGE_TYPES.TEXT,
+    sender: MESSAGE_SENDER.CHATBOT,
     inputLock: false,
-    quickReply: [],
+    quickReplies: [],
     skipLS: false,
     send_variable_to_apiai: false,
     sendVariableToLS: false,
@@ -17,6 +17,7 @@ export const web = [
     timestamp: new Date(),
   },
   {
+    type: MESSAGE_TYPES.TEXT_WITH_BUTTONS,
     sender: MESSAGE_SENDER.CHATBOT,
     inputLock: false,
     quickReplies: [],
@@ -24,7 +25,6 @@ export const web = [
     send_variable_to_apiai: false,
     sendVariableToLS: false,
     variable_name: 'subscription',
-    type: MESSAGE_TYPES.TEXT_WITH_BUTTONS,
     delay: 0,
     payload: {
       title: "",
@@ -53,10 +53,10 @@ export const web = [
 
 export const android = [
   {
-    sender: MESSAGE_SENDER.CHATBOT,
     type: MESSAGE_TYPES.TEXT,
+    sender: MESSAGE_SENDER.CHATBOT,
     inputLock: false,
-    quickReply: [],
+    quickReplies: [],
     skipLS: false,
     send_variable_to_apiai: false,
     sendVariableToLS: false,
@@ -68,6 +68,7 @@ export const android = [
     timestamp: new Date(),
   },
   {
+    type: MESSAGE_TYPES.TEXT_WITH_BUTTONS,
     sender: MESSAGE_SENDER.CHATBOT,
     inputLock: false,
     quickReplies: [],
@@ -75,7 +76,6 @@ export const android = [
     send_variable_to_apiai: false,
     sendVariableToLS: false,
     variable_name: 'subscription',
-    type: MESSAGE_TYPES.TEXT_WITH_BUTTONS,
     delay: 0,
     payload: {
       title: "",
@@ -99,26 +99,27 @@ export const android = [
       ]
     },
     timestamp: new Date(),
-  },
+  }
 ];
 
 export const hindi = [
   {
-    sender: MESSAGE_SENDER.CHATBOT,
     type: MESSAGE_TYPES.TEXT,
+    sender: MESSAGE_SENDER.CHATBOT,
     inputLock: false,
-    quickReply: [],
+    quickReplies: [],
     skipLS: false,
     send_variable_to_apiai: false,
     sendVariableToLS: false,
     variable_name: '',
     delay: 0,
     payload: {
-      text: "नमस्ते, मैं वीआईसी, आपका व्यक्तिगत वोडाफोन आइडिया असिस्टेंट हूं।"
+      text: "Hi, I'm VIC, Your Personal Vodafone Idea Assistant."
     },
     timestamp: new Date(),
   },
   {
+    type: MESSAGE_TYPES.TEXT_WITH_BUTTONS,
     sender: MESSAGE_SENDER.CHATBOT,
     inputLock: false,
     quickReplies: [],
@@ -126,30 +127,28 @@ export const hindi = [
     send_variable_to_apiai: false,
     sendVariableToLS: false,
     variable_name: 'subscription',
-    type: MESSAGE_TYPES.TEXT_WITH_BUTTONS,
     delay: 0,
     payload: {
       title: "",
-      subtitle: "आप आज क्या तलाशना चाहेंगे?",
+      subtitle: "What would you like to explore today?",
       buttons: [
         {
           type: BUTTON_TYPES.DEFAULT,
-          text: "प्रीपेड",
+          text: "Prepaid",
           url: ""
         },
         {
           type: BUTTON_TYPES.DEFAULT,
-          text: "पोस्टपेड",
+          text: "Postpaid",
           url: ""
         },
         {
           type: BUTTON_TYPES.DEFAULT,
-          text: "वोडाफोन आइडिया परिवार में शामिल हों",
+          text: "Join Vodafone Idea Family",
           url: ""
         }
       ]
     },
     timestamp: new Date(),
-  },
+  }
 ];
-
