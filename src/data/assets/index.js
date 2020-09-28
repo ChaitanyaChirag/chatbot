1314c0b4ddd78579c940d8c217ccb8a911cec82e
priyanshitaneja <priyanshi.taneja@oriserve.com>
2020-09-28 17:34:01 +0530

    ratingItem emojis added

--- src/data/assets/index.js
+++ src/data/assets/index.js
@@ -3,11 +3,17 @@ const logo = require('./logo.png')
 const trigger = require('./trigger.png')
 const poweredby = require('./poweredby.png')
 const lottiejson = require('./lottie.json')
+const RatingItem_sad = require('./RatingItem-sad.jpg')
+const RatingItem_normal = require('./RatingItem-normal.jpeg')
+const RatingItem_happy = require('./RatingItem-happy.jpg')
 
 module.exports = {
   background,
   logo,
   trigger,
   poweredby,
-  lottiejson
+  lottiejson,
+  RatingItem_sad,
+  RatingItem_normal,
+  RatingItem_happy,
 }
