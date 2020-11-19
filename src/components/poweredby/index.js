ee92096a656bb5a20d67345f103bd8f5509b6fdb
Vivek Vaibhav <vivek@oriserve.com>
2020-11-19 21:03:29 +0530

    PowerBy url click issue fixed

--- src/components/poweredby/index.js
+++ src/components/poweredby/index.js
@@ -7,18 +7,26 @@ import { chatbot_setting, translator } from '../../data/config/urls'
 import { LangContext } from '../../modules/context'
 
 const PoweredBy = React.memo(({ container_class }) => {
+  const goToUrl = () => {
+    if (chatbot_setting.powered_by.target_url) {
+      let win = window.open(chatbot_setting.powered_by.target_url, '_blank');
+      win.focus()
+    }
+  }
+
   return (
     <LangContext.Consumer>
       {
         lang => (
-          <a
-            href={chatbot_setting.powered_by.target_url}
-            target="blank"
-            className={`ori-animated ori-fade-in-up ${container_class}`}
-          >
+          <p className={`ori-animated ori-fade-in-up ${container_class}`}>
             <span className="ori-font-xxs ori-font-light">{translator.text[lang].poweredBy}</span>
-            <img src={poweredby} className="ori-l-mrgn-5 ori-height-10 ori-cursor-ptr" alt="" />
-          </a>
+            <img
+              src={poweredby}
+              className="ori-l-mrgn-5 ori-height-10 ori-cursor-ptr"
+              alt=""
+              onClick={goToUrl}
+            />
+          </p>
         )
       }
     </LangContext.Consumer>
