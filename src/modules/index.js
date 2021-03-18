e255167fa8a8820710f98de150074a46cb3634e1
Vivek Vaibhav <vivek@oriserve.com>
2021-03-18 10:31:47 +0530

    console added

--- src/modules/index.js
+++ src/modules/index.js
@@ -438,6 +438,7 @@ class AppContainer extends Component {
               }
             } else {
               if (data.button.target === 'newwindow') {
+                console.log('open link in new window')
                 window.open(data.button.url, 'newwindow', `width=${chatbot_setting.new_window_positon_and_size.width},height=${chatbot_setting.new_window_positon_and_size.height},top=${chatbot_setting.new_window_positon_and_size.top},left=${chatbot_setting.new_window_positon_and_size.left} `);
               } else {
                 window.open(data.button.url, '_blank');
