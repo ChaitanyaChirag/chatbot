ecc7a8f48cfd58397ef603c70c8fba720cb8c21f
Vivek Vaibhav <vivek@oriserve.com>
2021-08-25 16:29:08 +0530

    urls updated

--- src/data/config/urls.js
+++ src/data/config/urls.js
@@ -1,8 +1,8 @@
-export const socketUrl = "https://vodafone-dev.oriserve.in/liveConversations";
-export const networkCheckUrl = "https://google.com/";
-export const brandName = "vodafone";
-export const botName = "vodafone";
-export const role = "user";
-export const version = 1.1;
-export const senderId = "dish_chat_client";
-export const ga4MeasurementId = "G-D01BCJ2CJ1";
+export const socketUrl = "https://vodafone-dev.oriserve.in/liveConversations"
+export const networkCheckUrl = "https://google.com/"
+export const brandName = "vodafone"
+export const botName = "vodafone"
+export const role = "user"
+export const version = 1.1
+export const senderId = "dish_chat_client"
+export const ga4MeasurementId = "ENTER_GA4_MEASUREMENT_ID"
