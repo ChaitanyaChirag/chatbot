c6221ed88a36995c2760c1c1ef2a9bdaf72e27c4
Vivek Vaibhav <vivek@oriserve.com>
2021-08-25 13:11:07 +0530

    Menu Component updated

--- src/modules/chatbot/components/menu/index.js
+++ src/modules/chatbot/components/menu/index.js
@@ -1,50 +1,53 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import SecurityIcon from 'react-icons/lib/md/security';
-import InfoIcon from 'react-icons/lib/md/assignment';
-import FeedbackIcon from 'react-icons/lib/md/feedback';
-import ClearIcon from 'react-icons/lib/md/clear-all';
-import CloseIcon from 'react-icons/lib/md/close'
+import React from "react"
+import PropTypes from "prop-types"
+import SecurityIcon from "react-icons/lib/md/security"
+import InfoIcon from "react-icons/lib/md/assignment"
+import FeedbackIcon from "react-icons/lib/md/feedback"
+import ClearIcon from "react-icons/lib/md/clear-all"
+import CloseIcon from "react-icons/lib/md/close"
 
-import './index.scss';
+import "./index.scss"
 
-import { chatbot_setting, translator } from '../../../../data/config/brandSetup';
-import { INFO_CONTENT_TYPE, GOOGLE_ENABLER_EVENTS, CHATBOT_TYPE } from '../../../../data/config/constants'
+import { chatbot_setting, translator } from "../../../../data/config/brandSetup"
+import { INFO_CONTENT_TYPE, GOOGLE_ENABLER_EVENTS, CHATBOT_TYPE } from "../../../../data/config/constants"
 
 import { LangContext } from "../../../context"
 
-import DelayComponent from '../../../../components/delaycomponent';
+import DelayComponent from "../../../../components/delaycomponent"
 
 class MenuComponent extends React.PureComponent {
   onClickPrivacyPolicy = () => {
     this.props.showInfoContent(INFO_CONTENT_TYPE.PRIVACY_POLICY)
     if (chatbot_setting.chatbot_type === CHATBOT_TYPE.ADSTER && window.parent)
       window.parent.postMessage({
-        type: 'counter',
+        type: "counter",
         func: GOOGLE_ENABLER_EVENTS.PRIVACY_POLICY,
         message: ""
-      }, '*')
+      }, "*")
   }
 
   onClickTermsAndConditions = () => {
     this.props.showInfoContent(INFO_CONTENT_TYPE.TERMS_AND_CONDITIONS)
     if (chatbot_setting.chatbot_type === CHATBOT_TYPE.ADSTER && window.parent)
       window.parent.postMessage({
-        type: 'counter',
+        type: "counter",
         func: GOOGLE_ENABLER_EVENTS.TERMS_AND_CONDITIONS,
         message: ""
-      }, '*')
+      }, "*")
   }
 
   render() {
-    let { handleResetChat, isMounted, closeMenu, showFeedback, handleEndChat } = this.props;
+    let { handleResetChat, isMounted, closeMenu, showFeedback, handleEndChat } = this.props
     if (chatbot_setting.menu)
       return (
         <LangContext.Consumer>
           {
             lang => (
               <React.Fragment>
-                <div className={`ori-absolute ori-animated ori-animation-half menuOverlay ${isMounted ? "ori-fade-in" : "ori-fade-out" }`} onClick={closeMenu}> &nbsp;
+                <div
+                  className={`ori-absolute ori-animated ori-animation-half menuOverlay ${isMounted ? "ori-fade-in" : "ori-fade-out"}`}
+                  onClick={closeMenu}
+                > &nbsp;
                 </div>
                 <div className={`ori-animated ori-animation-half ori-absolute ori-pad-10 oriMenuContainer ${isMounted ? "ori-zoom-in-bottom-left" : "ori-zoom-out-bottom-left"}`}>
                   {
@@ -87,17 +90,17 @@ class MenuComponent extends React.PureComponent {
             )
           }
         </LangContext.Consumer>
-      );
-    return null;
+      )
+    return null
   }
 }
-const Menu = DelayComponent(MenuComponent);
+const Menu = DelayComponent(MenuComponent)
 
 Menu.propTypes = {
   closeMenu: PropTypes.func,
   handleResetChat: PropTypes.func,
   showFeedback: PropTypes.func,
   showInfoContent: PropTypes.func
-};
+}
 
-export default Menu;
+export default Menu
