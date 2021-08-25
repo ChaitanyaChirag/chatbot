5e38a2b2514e474c092e8255163c18bb1c97d6ce
Vivek Vaibhav <vivek@oriserve.com>
2021-08-25 13:14:41 +0530

    PreviewFile updated

--- src/modules/chatbot/components/previewfile/index.js
+++ src/modules/chatbot/components/previewfile/index.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Button from 'antd/lib/button';
+import React from "react"
+import PropTypes from "prop-types"
+import Button from "antd/lib/button"
 
-import { translator } from '../../../../data/config/brandSetup';
+import { translator } from "../../../../data/config/brandSetup"
 
-import { LangContext } from '../../../context'
+import { LangContext } from "../../../context"
 
-import DelayComponent from '../../../../components/delaycomponent';
+import DelayComponent from "../../../../components/delaycomponent"
 
 class PreviewFile extends React.PureComponent {
   componentWillUnmount() {
-    this.props.onFileRemove();
-  };
+    this.props.onFileRemove()
+  }
 
   renderImage = lang => {
-    const { file, fileUrl } = this.props;
-    if (file && file.type && file.type.indexOf('image/') !== -1) {
+    const { file, fileUrl } = this.props
+    if (file && file.type && file.type.indexOf("image/") !== -1) {
       return (
-        <div className="ori-bg-size-cover ori-bg-no-repeat" style={{ backgroundImage: `url(${fileUrl})`, height: '200px' }} />
-      );
+        <div className="ori-bg-size-cover ori-bg-no-repeat" style={{ backgroundImage: `url(${fileUrl})`, height: "200px" }} />
+      )
     }
     return (
-      <div className="ori-pad-10 ori-flex-row ori-flex-center ori-font-light" style={{ fontSize: '3rem', height: '150px' }}>{translator.text[lang].fileSelected}</div>
+      <div className="ori-pad-10 ori-flex-row ori-flex-center ori-font-light" style={{ fontSize: "3rem", height: "150px" }}>{translator.text[lang].fileSelected}</div>
     )
-  };
+  }
 
   render() {
-    const { isMounted, file, fileUrl, is_internet_connected, onClickCancel, onClickSend } = this.props;
+    const { isMounted, file, fileUrl, is_internet_connected, onClickCancel, onClickSend } = this.props
     return (
       <LangContext.Consumer>
         {
           lang => (
-            <div className={`ori-absolute ori-animated ori-animation-half ori-bg-black-light ori-align-full ori-z-index-99995 ori-pad-20 ori-flex-column ori-flex-center ${isMounted ? "ori-fade-in": "ori-fade-out"}`}>
+            <div className={`ori-absolute ori-animated ori-animation-half ori-bg-black-light ori-align-full ori-z-index-99995 ori-pad-20 ori-flex-column ori-flex-center ${isMounted ? "ori-fade-in" : "ori-fade-out"}`}>
               <div className="ori-border-radius-light ori-border-radius-5 ori-bg-white ori-b-mrgn-10 ori-overflow-hidden ori-bg-position-center ori-full-width">
                 {this.renderImage(lang)}
                 {
@@ -42,14 +42,27 @@ class PreviewFile extends React.PureComponent {
                 }
               </div>
               <div className="ori-flex-row ori-flex-jc ori-t-pad-15">
-                <Button className="ori-lr-mrgn-10 ori-lr-pad-15 ori-btn-ghost-primary" size="small" onClick={onClickCancel}>{translator.text[lang].cancel}</Button>
-                <Button className={`ori-lr-mrgn-10 ori-lr-pad-20 ${ is_internet_connected ? "ori-btn-fill-primary" :""}`} size="small" disabled={!is_internet_connected} onClick={onClickSend}>{is_internet_connected ? translator.text[lang].send : translator.text[lang].connecting}</Button>
+                <Button
+                  className="ori-lr-mrgn-10 ori-lr-pad-15 ori-btn-ghost-primary"
+                  size="small"
+                  onClick={onClickCancel}
+                >
+                  {translator.text[lang].cancel}
+                </Button>
+                <Button
+                  className={`ori-lr-mrgn-10 ori-lr-pad-20 ${is_internet_connected ? "ori-btn-fill-primary" : ""}`}
+                  size="small"
+                  disabled={!is_internet_connected}
+                  onClick={onClickSend}
+                >
+                  {is_internet_connected ? translator.text[lang].send : translator.text[lang].connecting}
+                </Button>
               </div>
             </div>
           )
         }
       </LangContext.Consumer>
-    );
+    )
   }
 }
 
@@ -60,10 +73,10 @@ PreviewFile.propTypes = {
   onClickCancel: PropTypes.func,
   onClickSend: PropTypes.func,
   is_internet_connected: PropTypes.bool
-};
+}
 
 PreviewFile.defaultProps = {
   is_internet_connected: false,
 }
 
-export default DelayComponent(PreviewFile);
+export default DelayComponent(PreviewFile)
